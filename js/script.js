04b82e7472ec3c544003e70124812016aa772e8f
Gustavo Feriani <guuh.feriani@gmail.com>
2025-09-06 10:55:09 -0300

    update btn scroll

--- js/script.js
+++ js/script.js
@@ -112,4 +112,23 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+
+    //Comentario em Linha
+    const btnTopo = document.getElementById("btn-topo");
+
+    window.onscroll = function () {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            btnTopo.style.display = "block";
+        } else {
+            btnTopo.style.display = "none";
+        }
+    };
+
+    btnTopo.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+
 });
